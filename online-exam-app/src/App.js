14fca16903bb3f2e6f8c0c62712ccf95e3ea2c3e
unknown <sreelash.18@gmail.com>
2023-12-06 15:30:23 +0530

    Login Ready!

--- online-exam-app/src/App.js
+++ online-exam-app/src/App.js
@@ -1,21 +1,15 @@
-import "./App.css";
+import "../src/components/loginSection/Login.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/headerSection/Header";
+import Login from "./components/loginSection/Login";
+import Footer from "./components/footerSection/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Login />
+      <Footer />
     </div>
   );
 }
