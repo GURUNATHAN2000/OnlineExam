5a8dd44877e2cbaf236fd439776a9b27a1729490
unknown <gurunathan30122000@gmail.com>
2023-12-07 16:44:56 +0530

    done

--- online-exam-app/src/App.js
+++ online-exam-app/src/App.js
@@ -7,24 +7,14 @@ import Footer from "./components/footerSection/Footer";
 import AdminHome from "./components/adminDashboard/AdminHome";
 import { useState } from "react";
 
-
-
 function App() {
   const [page, setPage] = useState("");
   return (
     <div>
-<<<<<<< HEAD
-       <Header/>
-       
-        <Login/> 
-       
-       <Footer/> 
-=======
       <Header page={page} />
       <Login setPage={setPage} />
       {/* <AdminHome setPage={setPage} /> */}
       <Footer />
->>>>>>> e67ba11b6ae5f33e08d6d394578c94c0436cdec5
     </div>
   );
 }
